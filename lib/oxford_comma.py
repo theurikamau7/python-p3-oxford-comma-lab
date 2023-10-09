fafd8dc1b445d37edf5017a4a173fe313fbf2892
IanKamau <theurikamau7@gmail.com>
2023-10-09 22:44:55 +0300

    completed oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,12 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(elements):
+  if len(elements) == 0:
+    return ""
+  elif len(elements) == 1:
+    return elements[0]
+  else:
+    if len(elements) == 2:
+      return f"{elements[0]} and {elements[1]}"
+    else:
+      formatted_elements = ", ".join(elements[:-1])
+      return f"{formatted_elements}, and {elements[-1]}"
+
